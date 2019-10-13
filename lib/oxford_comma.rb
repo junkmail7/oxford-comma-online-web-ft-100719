59b275e58b5f758961357da6bf6eeae12ba1cbf2
William Pannell <lpann5@gmail.com>
2019-10-13 23:25:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+array.each do |action|
+  if array.size==1
+    return array.join
+  elsif array.size==2
+    return array.join(" and ")
+  else
+    nuguy=[]
+    x=0
+    array.each do |ohyea|
+      if array[x]!=array.last
+      nuguy[x]=array[x]+", "
+      else
+      nuguy[x]="and "+array[x]
+      end
+      x=x+1
+    end
+    return nuguy.join
+  end
+end
 end
