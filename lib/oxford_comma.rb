2ed1e5eaf18d6e2b58369d103b35d354bbb1c08a
William Pannell <lpann5@gmail.com>
2019-10-13 23:07:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-array
+array.each do |action|
+  if array.size==1
+    return array.join
+  elsif array.size==2
+    return array.join(" and ")
+  end
+end
   
-return array.join(" and ")
+
 end
