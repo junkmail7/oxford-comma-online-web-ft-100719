2949305138cc3a836a583da04952c91a7b3fdb46
William Pannell <lpann5@gmail.com>
2019-10-13 23:25:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ array.each do |action|
     return array.join
   elsif array.size==2
     return array.join(" and ")
-  elsif array.size==3
-  return array[0] + ", " + array[1] + ", and " + array[2]
+  
   else
     nuguy=[]
     x=0
