e42444cd4b7fa88cb91d75955dcd1e4e274845ca
William Pannell <lpann5@gmail.com>
2019-10-13 23:18:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,14 +7,15 @@ array.each do |action|
   elsif array.size==3
   return array[0] + ", " + array[1] + ", and " + array[2]
   else
-  nuguy=[]
-  x=0
-  array.each do |ohyea|
-  if array[x]!=array.last
-  nuguy[x]=ohyea+", "
-  else
-  nuguy[x]=", and "+ohyea
+    nuguy=[]
+    x=0
+    array.each do |ohyea|
+    if array[x]!=array.last
+    nuguy[x]=ohyea+", "
+    else
+    nuguy[x]=", and "+ohyea
+    end
+    return nuguy.join
   end
-  return nuguy.join
 end
 end
